c87ca4de9342b74024254f1b8435a45830163466
sandeepgunturi <57071293+sandeepgunturi@users.noreply.github.com>
2020-02-23 23:42:17 +0530

    Update server.c

--- Assignment1/server.c
+++ Assignment1/server.c
@@ -1,157 +1,105 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <signal.h>
+// Server side C/C++ program to demonstrate Socket programming
 #include <unistd.h>
-#include <sys/types.h>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include "proto.h"
-#include "server.h"
-#include<time.h>
+#include <string.h>
+#include<pthread.h>
+#define PORT 8888
+int arr_end=0;
+
+struct client_node{
+  int sock;
+  int valread;
+};
 
 
+struct client_node client_node_arr[100];
+char* buffer_server[1024];
 
-// Global variables
-int server_sockfd = 0, client_sockfd = 0;
-ClientList *root, *now;
-
-void catch_ctrl_c_and_exit(int sig) {
-    ClientList *tmp;
-    while (root != NULL) {
-        printf("\nClose socketfd: %d\n", root->data);
-        close(root->data); // close all socket include server_sockfd
-        tmp = root;
-        root = root->link;
-        free(tmp);
-    }
-    printf("Bye\n");
-    exit(EXIT_SUCCESS);
+void sendtoall(char* msg){
+  for(int j=0;j<arr_end;j++){
+    send(client_node_arr[j].sock , msg , strlen(msg) , 0 );
+  }
 }
 
-void send_to_all_clients(ClientList *np, char tmp_buffer[]) {
-    ClientList *tmp = root->link;
-    while (tmp != NULL) {
-        if (np->data != tmp->data) { // all clients except itself.
-            printf("Send to sockfd %d: \"%s\" \n", tmp->data, tmp_buffer);
-            send(tmp->data, tmp_buffer, LENGTH_SEND, 0);
-        }
-        tmp = tmp->link;
-    }
-}
-
-void client_handler(void *p_client) {
-    int leave_flag = 0;
-    char nickname[LENGTH_NAME] = {};
-    char recv_buffer[LENGTH_MSG] = {};
-    char send_buffer[LENGTH_SEND] = {};
-    ClientList *np = (ClientList *)p_client;
-
-    // Naming
-    if (recv(np->data, nickname, LENGTH_NAME, 0) <= 0 || strlen(nickname) < 2 || strlen(nickname) >= LENGTH_NAME-1) {
-        printf("%s didn't input name.\n", np->ip);
-        leave_flag = 1;
-    } else {
-        strncpy(np->name, nickname, LENGTH_NAME);
-        printf("%s(%s)(%d) join the chatroom.\n", np->name, np->ip, np->data);
-        sprintf(send_buffer, "%s(%s) join the chatroom.", np->name, np->ip);
-	clock_t t1,t2;
-	t1=clock();
-	printf("time start is %li",t1);
-        send_to_all_clients(np, send_buffer);
-	t2=clock();
-	printf("time start is %li",t2);
-	    
-	}
-
-    // Conversation
-    while (1) {
-        if (leave_flag) {
-            break;
-        }
-        int receive = recv(np->data, recv_buffer, LENGTH_MSG, 0);
-        if (receive > 0) {
-            if (strlen(recv_buffer) == 0) {
-                continue;
+void* client_handler(void* temp){
+  while(1){
+    struct client_node temp2 = *((struct client_node *)temp);
+    //int a = *((int *) temp);
+    //free(temp);
+    //printf("%d\n",temp2.sock );
+    int valread = read( temp2.sock, buffer_server, 1024);
+    printf("%s\n",buffer_server);
+    if(buffer_server!=NULL){
+      if(buffer_server != "bye"){
+        sendtoall(buffer_server);
+      }
+      else{
+        //remove from array;
+        int start = 0;
+        while(start<=arr_end){
+          if(client_node_arr[start].sock == temp2.sock){
+            int t1 = start;
+            while(t1<arr_end-1){
+              client_node_arr[t1] = client_node_arr[t1+1];
             }
-            sprintf(send_buffer, "%s：%s from %s", np->name, recv_buffer, np->ip);
-        } else if (receive == 0 || strcmp(recv_buffer, "exit") == 0) {
-            printf("%s(%s)(%d) leave the chatroom.\n", np->name, np->ip, np->data);
-            sprintf(send_buffer, "%s(%s) leave the chatroom.", np->name, np->ip);
-            leave_flag = 1;
-        } else {
-            printf("Fatal Error: -1\n");
-            leave_flag = 1;
+            break;
+          }
+          start++;
         }
-        send_to_all_clients(np, send_buffer);
+      }
     }
-
-    // Remove Node
-    close(np->data);
-    if (np == now) { // remove an edge node
-        now = np->prev;
-        now->link = NULL;
-    } else { // remove a middle node
-        np->prev->link = np->link;
-        np->link->prev = np->prev;
-    }
-    free(np);
+  }
+  free(temp);
+  return NULL;
 }
 
-int main()
-{
-    signal(SIGINT, catch_ctrl_c_and_exit);
+int main(int argc, char const *argv[]) {
+  int server_fd, new_socket, valread;
+	struct sockaddr_in address;
+  int opt = 1;
+	int addrlen = sizeof(address);
+  // Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+  // Forcefully attaching socket to the port 8888
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt))){
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+  address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+  // Forcefully attaching socket to the port 8888
+  if (bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0){
+    perror("bind failed");
+    exit(EXIT_FAILURE);
+  }
+  //listen
+  if (listen(server_fd, 3) < 0){
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+  while(1){
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0){
+  		perror("accept");
+  		exit(EXIT_FAILURE);
+  	}
+    //printf("connected\n");
+    //printf("%d\n",new_socket);
+    struct client_node* temp = (struct client_node*)malloc(sizeof(struct client_node));
+    temp->sock = new_socket;
 
-    // Create socket
-    server_sockfd = socket(AF_INET , SOCK_STREAM , 0);
-    if (server_sockfd == -1) {
-        printf("Fail to create a socket.");
-        exit(EXIT_FAILURE);
-    }
+    client_node_arr[arr_end] = *temp;
+    //printf("%d\n",arr_end );
+    pthread_t client_listen_always;
+    pthread_create(&client_listen_always,NULL,client_handler,temp);
+    arr_end++;
+  }
 
-    // Socket information
-    struct sockaddr_in server_info, client_info;
-    int s_addrlen = sizeof(server_info);
-    int c_addrlen = sizeof(client_info);
-    memset(&server_info, 0, s_addrlen);
-    memset(&client_info, 0, c_addrlen);
-    server_info.sin_family = PF_INET;
-    server_info.sin_addr.s_addr = INADDR_ANY;
-    server_info.sin_port = htons(8888);
-
-    // Bind and Listen
-    bind(server_sockfd, (struct sockaddr *)&server_info, s_addrlen);
-    listen(server_sockfd, 5);
-
-    // Print Server IP
-    getsockname(server_sockfd, (struct sockaddr*) &server_info, (socklen_t*) &s_addrlen);
-    printf("Start Server on: %s:%d\n", inet_ntoa(server_info.sin_addr), ntohs(server_info.sin_port));
-
-    // Initial linked list for clients
-    root = newNode(server_sockfd, inet_ntoa(server_info.sin_addr));
-    now = root;
-
-    while (1) {
-        client_sockfd = accept(server_sockfd, (struct sockaddr*) &client_info, (socklen_t*) &c_addrlen);
-
-        // Print Client IP
-        getpeername(client_sockfd, (struct sockaddr*) &client_info, (socklen_t*) &c_addrlen);
-        printf("Client %s:%d come in.\n", inet_ntoa(client_info.sin_addr), ntohs(client_info.sin_port));
-
-        // Append linked list for clients
-        ClientList *c = newNode(client_sockfd, inet_ntoa(client_info.sin_addr));
-        c->prev = now;
-        now->link = c;
-        now = c;
-
-        pthread_t id;
-        if (pthread_create(&id, NULL, (void *)client_handler, (void *)c) != 0) {
-            perror("Create pthread error!\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    return 0;
+  return 0;
 }
